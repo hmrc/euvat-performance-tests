92162ac38339e12c35ec51177677638b9bb9d81b
JohnWhitfield <22446460+JohnWhitfield@users.noreply.github.com>
2026-05-15 11:23:30 +0100

    Update to contact details page

--- src/test/scala/uk/gov/hmrc/perftests/euvat/simulations/EUVATSimulation.scala
+++ src/test/scala/uk/gov/hmrc/perftests/euvat/simulations/EUVATSimulation.scala
@@ -41,7 +41,7 @@ class EUVATSimulation extends Simulation with PerformanceTestRunner {
     getWhatIsTheRefundPeriod,
     postWhatIsTheRefundPeriod("08", "2025", "12", "2025"),
     getWhoShouldWeContactAboutThisClaim,
-    postWhoShouldWeContactAboutThisClaim("Test123@test.com", "FirstNameTest123", "LastNameTest123", "01234567890"),
+    postWhoShouldWeContactAboutThisClaim("Test123@test.com", "01234567890"),
     getAddBusinessActivityForThisClaim,
     postAddBusinessActivityForThisClaim("yes"),
     getBusinessActivityCodeThree,

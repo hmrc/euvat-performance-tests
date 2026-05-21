e1be93bddb1aa7fa25c2a2f821392b0f5983412d
sultanwasimraj <258210018+sultanwasimraj@users.noreply.github.com>
2026-05-21 11:22:59 +0100

    Uncommenting few scripts as part of DTR-5142

--- src/test/scala/uk/gov/hmrc/perftests/euvat/requests/MakeNewEUVATClaimRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/euvat/requests/MakeNewEUVATClaimRequests.scala
@@ -117,13 +117,13 @@ object MakeNewEUVATClaimRequests extends ServicesConfiguration with EUVATPerform
 
   val getAddingBusinessActivityCodeTwo: HttpRequestBuilder =
     http("[get ] Add a 2nd business activity code page")
-      .get(euvatFilingFrontendUrl + "/business-activity-code-two")
+      .get(euvatFilingFrontendUrl + "/business-activity-code-2")
       .check(status.is(200))
       .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
 
   def postAddingBusinessActivityCodeTwo(businessActivityCode2: String): HttpRequestBuilder =
     http("[post] Add a 2nd business activity code page")
-      .post(euvatFilingFrontendUrl + "/business-activity-code-two")
+      .post(euvatFilingFrontendUrl + "/business-activity-code-2")
       .formParam("value", businessActivityCode2)
       .formParam("csrfToken", f"#{csrfToken}")
       .check(status.is(303))

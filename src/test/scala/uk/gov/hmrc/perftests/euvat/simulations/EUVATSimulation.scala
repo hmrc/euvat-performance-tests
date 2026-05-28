404df2ab7863d653604d2527c461247bb9ba1f0e
Srikanth Madduri <srikanth.madduri@digital.hmrc.gov.uk>
2026-05-28 14:08:52 +0100

    DTR-4289 Add tests for invoice number page

--- src/test/scala/uk/gov/hmrc/perftests/euvat/simulations/EUVATSimulation.scala
+++ src/test/scala/uk/gov/hmrc/perftests/euvat/simulations/EUVATSimulation.scala
@@ -60,9 +60,9 @@ class EUVATSimulation extends Simulation with PerformanceTestRunner {
     getWhatIsTheSuppliersName,
     postWhatIsTheSuppliersName("Testing"),
     getWhatIsTheSuppliersAddress,
-    postWhatIsTheSuppliersAddress("Test Address Line 1", "Test Address Line 2", "Test Address Line 3")
-//    getWhatIsTheInvoiceNumber,
-//    postWhatIsTheInvoiceNumber(""),
+    postWhatIsTheSuppliersAddress("Test Address Line 1", "Test Address Line 2", "Test Address Line 3"),
+    getWhatIsTheInvoiceNumber,
+    postWhatIsTheInvoiceNumber(""),
 //    getWhatIsTheInvoiceDate,
 //    postWhatIsTheInvoiceDate("15","05","2026")
   )

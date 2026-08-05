5daaa1b233d1e9ebceeaec98fe08588ca073afc6
Srikanth Madduri <srikanth.madduri@digital.hmrc.gov.uk>
2026-08-05 21:58:39 +0100

    clean up code

--- src/test/scala/uk/gov/hmrc/perftests/euvat/requests/PurchaseRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/euvat/requests/PurchaseRequests.scala
@@ -20,7 +20,6 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.gatling.http.request.builder.HttpRequestBuilder
 import uk.gov.hmrc.performance.conf.ServicesConfiguration
-import uk.gov.hmrc.perftests.euvat.requests.PurchaseRequests.getCheckVatClaim
 
 object PurchaseRequests extends ServicesConfiguration with EUVATPerformanceTestBase {
 

bd5c9642b4c24686be413b65d5d377713acf3562
JohnWhitfield <22446460+JohnWhitfield@users.noreply.github.com>
2026-09-14 19:30:13 +0100

    Removed duplicate method to getMakeANewEUVATClaimPage

--- src/test/scala/uk/gov/hmrc/perftests/euvat/requests/PurchaseRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/euvat/requests/PurchaseRequests.scala
@@ -606,10 +606,6 @@ object PurchaseRequests extends ServicesConfiguration with EUVATPerformanceTestB
       .formParam("csrfToken", f"#{csrfToken}")
       .check(status.is(303))
 
-  val getMakeANewEUVATClaimPage: HttpRequestBuilder =
-    http("[get ] Make a claim for an EU VAT refund page")
-      .get(euvatFilingFrontendUrl + "/make-a-claim-eu-vat-refund")
-      .check(status.is(200))
 
   val AddPurchaseJourney: List[HttpRequestBuilder] = List(
     getBeforeYouStart,
